878ff16ffc700add337194342b76f52e5cb2d34a
Oriol Burgaya <oriolbv@gmail.com>
2019-09-14 00:30:24 +0200

    Image resize added

--- whats-eat-ai/food_classification.py
+++ whats-eat-ai/food_classification.py
@@ -13,27 +13,20 @@ from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
 from skimage.transform import resize
 
-from flask import Flask
+model = load_model('food_mnist.h5py')
+images=[]
+filenames = ['img_carpaccio.jpg','img_apple_pie.jpg']
+for filepath in filenames:
+    image = plt.imread(filepath)
+    image_resized = resize(image, (80, 106),preserve_range=True)
+    images.append(image_resized)
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def food_classification():
-    model = load_model('food_mnist.h5py')
-    images=[]
-    filenames = ['img_carpaccio.jpg','img_apple_pie.jpg']
-    for filepath in filenames:
-        image = plt.imread(filepath)
-        image_resized = resize(image, (80, 106),preserve_range=True)
-        images.append(image_resized)
-
-    # List to numpy array
-    X = np.array(images, dtype=np.uint8)
-    test_X = X.astype('float32')
-    test_X = test_X / 255.
-    predicted_classes = model.predict(test_X)
-    prediction_1 = predicted_classes[0]
-    prediction_2 = predicted_classes[1]
-    print("Prediction 1: ", prediction_1)
-    print("Prediction 2: ", prediction_2)
+# List to numpy array
+X = np.array(images, dtype=np.uint8)
+test_X = X.astype('float32')
+test_X = test_X / 255.
+predicted_classes = model.predict(test_X)
+prediction_1 = predicted_classes[0]
+prediction_2 = predicted_classes[1]
+print("Prediction 1: ", prediction_1)
+print("Prediction 2: ", prediction_2)
---
+++ whats-eat-ai/image_resize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+from PIL import Image
+import os, sys
+
+path = "whats-eat-ai/img/"
+dirs = os.listdir( path )
+
+def resize():
+    for direc in dirs:
+        directory = os.listdir(path + direc)
+        path2 = path + direc + "/"
+        print(path2)
+        for item in directory:
+            print(path2 + item)
+            if os.path.isfile(path2+item):
+                im = Image.open(path2+item)
+                f, e = os.path.splitext(path2+item)
+                imResize = im.resize((64,64), Image.ANTIALIAS)
+                imResize.save(f + '.jpg', 'JPEG', quality=100)
+
+resize()
